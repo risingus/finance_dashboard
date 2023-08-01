17691ed02ffb2fee0a6f5cce496ccfb52152df35
Gustavo Lima <gustavo.it@outlook.com>
2023-08-01 19:18:57 -0300

    feat
    1. installed axios
    2. created currencyApi

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import { currencyApi } from "./services/apis"
 
 function App() {
+  async function test() {
+    try {
+      await currencyApi.get('/latest?amount=1&from=USD&to=BRL')
+
+    } catch (error) {
+      console.dir(error)
+    }
+  }
+
   return (
+    <div>
+      <button onClick={test}>
+        click
+      </button>
     <h3>here</h3>
+    </div>
   )
 }
 
