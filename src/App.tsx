89c090fd0a65967c2c352c26d8b9571b9d4ffc8a
Gustavo Lima <gustavo.it@outlook.com>
2023-08-08 17:00:53 -0300

    no time...

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
+import { useMemo } from 'react'
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
 import { CardCurrency } from './components/CardCurrency'
 import { ModalCreateCurrency } from './components/ModalCreateCurrency'
+import { AppLayout } from './layouts/AppLayout'
+import { useThemeSelector } from './hooks/useThemeSelector';
+import stylesDark from './styles/global-dark.module.scss';
+import stylesLight from './styles/global-light.module.scss';
 
 const queryClient = new QueryClient()
 
 function App() {
+  const isDarkTheme = useThemeSelector()
+
+  const activeStyles = useMemo(() => {
+    if (isDarkTheme) return stylesDark
+    return stylesLight
+  }, [isDarkTheme])
 
   return (
-    <QueryClientProvider client={queryClient}>
-    <div>
-        <CardCurrency from='USD' to='BRL' />
-        <ModalCreateCurrency />
+    <div className={activeStyles.body}>
+      <QueryClientProvider client={queryClient}>
+        <AppLayout>
+          <CardCurrency from='USD' to='BRL' />
+          <ModalCreateCurrency />
+        </AppLayout>
+      </QueryClientProvider>
     </div>
-    </QueryClientProvider>
   )
 }
 
--- src/components/ModalCreateCurrency/index.tsx
+++ src/components/ModalCreateCurrency/index.tsx
@@ -5,7 +5,7 @@ import './styles.scss'
 export const ModalCreateCurrency = () => (
   <Dialog.Root>
     <Dialog.Trigger asChild>
-      <button className="Button violet">Edit profile</button>
+      <button>Edit profile</button>
     </Dialog.Trigger>
     <Dialog.Portal>
       <Dialog.Overlay className="DialogOverlay" />
